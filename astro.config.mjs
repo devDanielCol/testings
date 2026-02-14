ba4933f095d7d274fa494412fb50c35b2ee99167
Daniel Fernando Cubillos Ramos <danielcubilllos@MacBook-Pro-de-Daniel.local>
2026-02-13 19:04:15 -0500

    added config to deploy on vercel server platform

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-    adapter: node({ mode: 'standalone' }),
+    output: 'server',
+    adapter: vercel({
+        edgeMiddleware: true,
+    }),
     integrations: [
         react(),
     ],
